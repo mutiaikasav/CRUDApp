bc6b4a3bf9ae1c89efc438cbf5205feff60f5043
Mutia Ika Savitri <mutiaika.svtr@gmail.com>
2022-06-16 21:56:03 +0700

    update division use dropdown

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -5,8 +5,16 @@ namespace CRUDApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Position { get; set; }
-        public string Division { get; set; }
+        public Div Division { get; set; }
         public string Address { get; set;}
         public string Gender { get; set;}
+    }
+
+    public enum Div
+    {
+        IT,
+        Marketing,
+        HRGA,
+        Accounting
     }
 }
